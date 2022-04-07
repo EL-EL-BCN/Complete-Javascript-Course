3e7e5dd9846e3fd5320bef4ffd82344c8e6085f9
EL-EL-BCN <elliottbcn@gmail.com>
2022-04-08 00:23:47 +0200

    end of study 07-07-22

---
+++ 01-fundamentals-part-1/script.js
@@ -0,0 +1,80 @@
+/*
+
+// let js = 'amazing';
+// if (js === 'amazing')
+//     alert("Javascript is fun!");
+
+// console.log(40 + 8 + 23 - 10);
+
+
+let js = 'amazing';
+console.log(40 + 9 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// The above are three types of value
+
+let fistName = "Bob";
+
+// Here we can assign a value to a variable
+
+console.log(fistName);
+console.log(fistName);
+console.log(fistName);
+
+// We can use a variable multiple times in our code
+
+// Conventions and rules for naming variables
+// We use cammel case. i.e. the first letter of each word after the first is capitalised with no spaces. i.e firstName, lastName, dateOfBirth etc...
+
+// Javascript has illegal variable names
+// i.e. let 3years = 3; Variable names cannot start with a number. Variable names can only have letters, numbers, underscores and dolar signs.
+
+// Javascript also has reserved keywords such as new. let new = 27; so they would also be illegal variable names. Another axample would be let function = 27;
+
+// let name = "Jonas"; name is a reserved keyword but javascript allows variables called name but it may cause problems later on. Better to use firstName.
+
+let myFirstJob = "coder";
+let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+let country = "Spain";
+let continent = "Europe";
+let population = "47 mil";
+
+console.log("My country is " + country + " with a population of " + population + " in the continent of " + continent);
+
+*/
+
+let firstName = "John"; // String
+let personAge = 23; // Number
+let isMember = true; // boolean
+
+console.log(typeof firstName);
+console.log(typeof personAge);
+console.log(typeof isMember);
+
+
+// javascriptIsFun variable initially is Boolean but changes to string.
+let javascriptIsFun = true;
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+// Undefined variable
+let year; // when we declare an undefined variable it will hold the value of undefined! And also have the type of Undefined.
+console.log(year);
+console.log(typeof year);
+
+year = 1992;
+console.log(typeof year);
+
+console.log(typeof null); // Nte that null is not null in JS because of a legacy bug, it shows as object!
+
+
+
+
+

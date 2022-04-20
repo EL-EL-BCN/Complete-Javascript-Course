680df2efc8f5ac57976492e5fa268dfa7bd36529
EL-EL-BCN <elliottbcn@gmail.com>
2022-04-20 22:23:43 +0200

    taking a pause

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -4,7 +4,25 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
+// DESTRUCTURING AN ARRAY EXAMPLE 1
+
+/* 
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+// Above we have an array that holds values 2, 3, and 4. WE can destructure it by declaring a const variable for each position in the array, but this is not very efficient.
+
+const [x, y, z] = arr;
+// We can also destructure an array like this, declaring all the variables in one line.
+
+console.log(a, b, c);
+console.log(x, y, z);
+// Note that destructuring an array does not destory it, we are only reading what is in the array.
+
+// DESTRUCTURING AN ARRAY EXAMPLE 2
+
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -27,3 +45,250 @@ const restaurant = {
     },
   },
 };
+
+*/
+
+// DESTRUCTURING AN ARRAY EXAMPLE 3
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+// const [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+// [main, , secondary] = [secondary, , main];
+// console.log(main, secondary);
+
+// DESTRUCTURING AN ARRAY EXAMPLE 4 - USING A FUNCTION
+
+/*
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+console.log(restaurant.order(2, 0));
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+*/
+
+/*
+
+// DESTRUCTURING AN ARRAY EXAMPLE 5 - FROM NESTED ARRAY
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// DESTRUCTURING AN ARRAY EXAMPLE 6 - DEFAULT VALUES
+
+// const [p, q, r] = [8, 9];
+// console.log(p, q, r);
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+*/
+
+/*
+
+// DESTRUCTURING AN OBJECT EXAMPLE 1
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+*/
+
+// DESTRUCTURING AN OBJECT EXAMPLE 2 - MUTATING VARIABLES
+
+/* 
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+*/
+
+// DESTRUCTURING AN OBJECT EXAMPLE 2 - NESTED OBJECT
+
+/*
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const { openingHours } = restaurant;
+const { fri } = openingHours;
+console.log(fri);
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+*/
+
+/*
+
+// DESTRUCTURING AN OBJECT EXAMPLE 2 - DE-STRUCTURING FUNCTION
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '23:30',
+  address: 'via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+
+*/
+
+// DESTRUCTURING AN OBJECT EXAMPLE 3 - DE-STRUCTURING FUNCTION WITH DEFAULT VALUES
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 1,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '23:30',
+  address: 'via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'via del Sole, 21',
+  starterIndex: 2,
+});

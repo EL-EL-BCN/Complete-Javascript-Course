61d92d6b3aefc096d252909a832274fe00486c6b
EL-EL-BCN <elliottbcn@gmail.com>
2022-05-19 23:11:35 +0200

    half way through chapter 14

--- 14-Object-Orientated-Programming/script.js
+++ 14-Object-Orientated-Programming/script.js
@@ -1,1 +1,166 @@
 'use strict';
+
+/*
+const Person = function (firstName, BirthYear) {
+  this.firstName = firstName;
+  this.BirthYear = BirthYear;
+};
+
+const jonas = new Person('Jonus', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.BirthYear);
+};
+
+jonas.calcAge();
+
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+
+const arr = [3, 6, 5, 7, 4, 5, 7, 5, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+*/
+
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+/*
+
+// 1.
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+// 2.
+Car.prototype.accelerate = function () {
+  console.log((this.speed += 10));
+};
+
+// 3.
+Car.prototype.brake = function () {
+  console.log((this.speed -= 5));
+};
+
+// 4.
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+console.log(bmw, mercedes);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
+
+*/
+
+/*
+// Class expression
+const PersonCl = class {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.BirthYear = birthYear;
+  }
+};
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+*/
+
+// class Declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.BirthYear = birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullname() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davies', 1996);
+console.log(jessica);
+
+/*
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+*/
